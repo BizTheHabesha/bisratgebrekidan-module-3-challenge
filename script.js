58da421ea233f19f11ef53ae51ab5e27999d0ccd
Biz <github@bisrat.dev>
2023-03-07 21:24:03 -0700

    add lookup table

--- script.js
+++ script.js
@@ -11,13 +11,29 @@ function writePassword() {
 }
 
 function generatePassword(){
-  var password;
-  var lc = document.querySelector('#lowercase');// bool
-  var up = document.querySelector('#uppercase');// bool
-  var num = document.querySelector('#numbers');// bool
-  var sc = document.querySelector('#length');// bool
+  var password = "";
+  // build char type lookup for later
+  var charTypeLookup = [];
+  // next to each if is the ascii associated with those chars
+  if(document.querySelector('#lowercase').checked){// 97-122
+    charTypeLookup.push('lc');
+  }
+  if(document.querySelector('#uppercase').checked){// 65-90
+    charTypeLookup.push('uc');
+  }
+  if(document.querySelector('#numbers').checked){// 48-57
+    charTypeLookup.push('num');
+  }
+  if(document.querySelector('#specialchars').checked){// 35-47 & 58-64 & 91-95 & 123-126
+    charTypeLookup.push('spec');
+  }
+
+  // go through psswrd char by char, for every char the user selected
   var len = document.querySelector('#length').value; // int
-  return password;
+  for(i=0; i<len; i++){
+    
+  }
+  return charTypeLookup;
 }
 
 // Add event listener to generate button
