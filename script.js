945a88a5be2dbaf6c497a01defe915800a709d2e
Biz <github@bisrat.dev>
2023-03-07 19:58:12 -0700

    add input to js vars

--- script.js
+++ script.js
@@ -11,6 +11,13 @@ function writePassword() {
 }
 
 function generatePassword(){
+  var password;
+  var lc = document.querySelector('#lowercase');// bool
+  var up = document.querySelector('#uppercase');// bool
+  var num = document.querySelector('#numbers');// bool
+  var sc = document.querySelector('#length');// bool
+  var len = document.querySelector('#length').value; // int
+  return password;
 }
 
 // Add event listener to generate button
